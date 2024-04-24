bdc13b3947bf358e459c5c9ed0941e90eba5489b
Brachya <brachyaa@gmail.com>
2024-04-24 19:59:39 +0300

    working compose docker with sample db

---
+++ client/db.py
@@ -0,0 +1,51 @@
+import mysql.connector
+
+
+class MyDb:
+    def __init__(
+        self,
+        host: str = "10.5.0.4",
+        port: int = 3306,
+        user: str = "brachya",
+        password: str = "1234",
+        database: str = "classicmodels",
+        table: str = "customers",
+    ) -> None:
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+        self.table = table
+        self.mydb = mysql.connector.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+        )
+        self.mycursor = self.mydb.cursor()
+
+    def query(self, search: str = "") -> tuple[bool, str]:
+        self.mycursor.execute(f"SHOW COLUMNS FROM {self.table};")
+        headers = self.mycursor.fetchall()
+        self.mycursor.execute(
+            f'SELECT * FROM {self.table} where customerName like "%{search}%" LIMIT 10'
+        )
+        data = self.mycursor.fetchall()
+        html = "<table\n"
+        html += "<tr>\n"
+        for head in headers:
+            html += f"<th>{head[0]}</th>\n"  # type:ignore
+        html += "</tr>\n"
+        for row in data:
+            html += "<tr>\n"
+            for val in row:
+                html += f"<td>{val}</td>\n"
+            html += "</tr>\n"
+        html += "</table>\n"
+        return (True if data else False, html)
+
+    def close(self) -> None:
+        self.mycursor.close()
+        self.mydb.close()
---
+++ client/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from db import MyDb
+
+
+class Server:
+    def __init__(self) -> None:
+        self.connected_to_db = False
+        self.app = FastAPI()
+        self.app.swagger_ui_parameters
+
+    async def connect_to_db(self) -> None:
+        try:
+            self.db = MyDb()
+            self.connected_to_db = True
+            print("database uploaded")
+        except:
+            print("Failed to connect the database")
+
+    def shut_down(self) -> None:
+        print("Closing database connection.")
+        self.db.close()
+
+    def run_server(self) -> None:
+        @self.app.get("/")
+        async def root():
+            return {"message": "HELLO", "connected to db": self.connected_to_db}
+
+        @self.app.get("/customers/{name}")
+        async def get_customer_by_name(name: str):
+            if not self.connected_to_db:
+                await self.connect_to_db()
+            exist, data = self.db.query(name)
+            if exist:
+                return HTMLResponse(data, 200)
+            return HTMLResponse("key doesn't match", 400)
+
+        @app.on_event("startup")
+        def startup():
+            print("startup")
+
+        @app.on_event("shutdown")
+        def shutdown():
+            self.shut_down()
+            print("shutdown")
+
+
+server = Server()
+app = server.app
+server.run_server()
